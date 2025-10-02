e33259d455c6ebccc527bd586b03e0e9bd5a4675
Bakii <tayolowosale@gmail.com>
2025-10-02 10:25:09 +0100

    fix: linting issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import {  PRODUCT_THREE } from "@/images";
+import { PRODUCT_THREE } from "@/images";
 import Image from "next/image";
 import "./globals.css";
 import Card from "@/components/Card";
@@ -11,29 +11,31 @@ import { SALE_ITEMS } from "@/utils";
 import { permanentMarker } from "@/components/Navbar";
 
 export default function Home() {
-  const [targetTime, setTargetTime] = useState<number | null>(null);
+	const [targetTime, setTargetTime] = useState<number | null>(null);
 
-  useEffect(() => {
-    setTargetTime(new Date("2025-05-01T00:00:00").getTime());
-  }, []);
+	useEffect(() => {
+		setTargetTime(new Date("2025-05-01T00:00:00").getTime());
+	}, []);
 
-  const [...otherItems] = SALE_ITEMS;
-  return (
-    <>
-      <main className="overflow-x-hidden flex flex-col items-center justify-center px-4 lg:px-8 py-4">
-        <section className="flex flex-col-reverse lg:flex lg:flex-row lg:h-[88dvh]">
-          <div className="flex flex-col justify-center items-center w-full lg:w-1/2 gap-10">
-            <aside className="text-left flex flex-col gap-8">
-              <div>
-                <h1 className="text-3xl font-extrabold">Redtech Bottle Water</h1>
-                <h2 className="italic">White Sail, Lightning & Chlorophyll</h2>
-              </div>
-              <p>
-                {`With a design inspired by performance gear, the Redtech Bottle brings a classic, elevated look to your everyday hydration. With premium materials, a sleek silhouette that stands out, and an ergonomic grip for a secure hold, this bottle blends versatility with signature style.`}
-              </p>
-            </aside>
+	const [...otherItems] = SALE_ITEMS;
+	return (
+		<>
+			<main className="overflow-x-hidden flex flex-col items-center justify-center px-4 lg:px-8 py-4">
+				<section className="flex flex-col-reverse lg:flex lg:flex-row lg:h-[88dvh]">
+					<div className="flex flex-col justify-center items-center w-full lg:w-1/2 gap-10">
+						<aside className="text-left flex flex-col gap-8">
+							<div>
+								<h1 className="text-3xl font-extrabold">
+									Redtech Bottle Water
+								</h1>
+								<h2 className="italic">White Sail, Lightning & Chlorophyll</h2>
+							</div>
+							<p>
+								{`With a design inspired by performance gear, the Redtech Bottle brings a classic, elevated look to your everyday hydration. With premium materials, a sleek silhouette that stands out, and an ergonomic grip for a secure hold, this bottle blends versatility with signature style.`}
+							</p>
+						</aside>
 
-            {/* {firstItem && (
+						{/* {firstItem && (
               <Card
                 imageSource={firstItem.imageSource}
                 cardTitle={firstItem.cardTitle}
@@ -44,76 +46,72 @@ export default function Home() {
                 discountedPrice={firstItem.discountedPrice}
               />
             )} */}
-          </div>
+					</div>
 
-          <aside className="w-full lg:w-1/2 flex flex-col justify-center items-center gap-6">
-            <div className="hidden lg:block">
-              {targetTime && <FlipClockCountdown to={targetTime} />}
-            </div>
+					<aside className="w-full lg:w-1/2 flex flex-col justify-center items-center gap-6">
+						<div className="hidden lg:block">
+							{targetTime && <FlipClockCountdown to={targetTime} />}
+						</div>
 
-            <div className="block lg:hidden">
-              {targetTime && (
-                <FlipClockCountdown
-                  to={targetTime}
-                  digitBlockStyle={{ width: 30, height: 50, fontSize: 24 }}
-                />
-              )}
-            </div>
+						<div className="block lg:hidden">
+							{targetTime && (
+								<FlipClockCountdown
+									to={targetTime}
+									digitBlockStyle={{ width: 30, height: 50, fontSize: 24 }}
+								/>
+							)}
+						</div>
 
-            <div className="animate-zoom">
-              <Image
-                alt="main product photo"
-                src={PRODUCT_THREE}
-                height={500}
-                width={500}
-                className="drop-shadow-customDrop z-10"
-              />
-            </div>
-          </aside>
-        </section>
+						<div className="animate-zoom">
+							<Image
+								alt="main product photo"
+								src={PRODUCT_THREE}
+								height={500}
+								width={500}
+								className="drop-shadow-customDrop z-10"
+							/>
+						</div>
+					</aside>
+				</section>
 
-         <div className="text-center space-y-2">
-            <h2 className={`${permanentMarker.className} text-2xl`}>
-              Exciting Offers
-            </h2>
-            <p>Do not miss these amazing deals!</p>
-          </div>
+				<div className="text-center space-y-2">
+					<h2 className={`${permanentMarker.className} text-2xl`}>
+						Exciting Offers
+					</h2>
+					<p>Do not miss these amazing deals!</p>
+				</div>
 
-        <section className="w-full flex flex-col gap-10 items-center justify-center lg:flex-row h-[90dvh] lg:h-[88dvh]">
-         
+				<section className="w-full flex flex-col gap-10 items-center justify-center lg:flex-row h-[90dvh] lg:h-[88dvh]">
+					<div className="hidden lg:flex gap-2 justify-evenly w-full">
+						{otherItems.map((item, index) => (
+							<Card
+								key={index}
+								imageSource={item.imageSource}
+								cardTitle={item.cardTitle}
+								cardDescription={item.cardDescription}
+								discountRate={item.discountRate}
+								discountedPrice={item.discountedPrice}
+								cardId={item.id}
+							/>
+						))}
+					</div>
 
-          <div className="hidden lg:flex gap-2 justify-evenly w-full">
-            {otherItems.map((item, index) => (
-              <Card
-                key={index}
-                imageSource={item.imageSource}
-                cardTitle={item.cardTitle}
-                cardDescription={item.cardDescription}
-                discountRate={item.discountRate}
-                discountedPrice={item.discountedPrice}
-                price={item.price}
-                cardId={item.id}
-              />
-            ))}
-          </div>
-
-          <div className="lg:hidden carousel carousel-center max-w-sm space-x-6 p-4">
-            {otherItems.map((item, index) => (
-              <div key={index} className="carousel-item">
-                <Card
-                  imageSource={item.imageSource}
-                  cardTitle={item.cardTitle}
-                  cardDescription={item.cardDescription}
-                  discountRate={item.discountRate}
-                  discountedPrice={item.discountedPrice}
-                  price={item.price}
-                  cardId={item.id}
-                />
-              </div>
-            ))}
-          </div>
-        </section>
-      </main>
-    </>
-  );
+					<div className="lg:hidden carousel carousel-center max-w-sm space-x-6 p-4">
+						{otherItems.map((item, index) => (
+							<div key={index} className="carousel-item">
+								<Card
+									imageSource={item.imageSource}
+									cardTitle={item.cardTitle}
+									cardDescription={item.cardDescription}
+									discountRate={item.discountRate}
+									discountedPrice={item.discountedPrice}
+									cardId={item.id}
+								/>
+							</div>
+						))}
+					</div>
+				</section>
+			</main>
+		</>
+	);
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,80 +5,74 @@ import CustomButton from "./CustomButton";
 import { useRouter } from "next/navigation";
 
 interface CardProps {
-  imageSource: StaticImageData;
-  cardTitle: string;
-  cardDescription: string;
-  onButtonClick?: () => void;
-  price: string;
-  discountedPrice: string;
-  discountRate: string;
-  cardId: string;
+	imageSource: StaticImageData;
+	cardTitle: string;
+	cardDescription: string;
+	onButtonClick?: () => void;
+	discountedPrice: string;
+	discountRate: string;
+	cardId: string;
 }
 
 const Card = ({
-  imageSource,
-  cardTitle,
-  cardDescription,
-  price,
-  discountedPrice,
-  discountRate,
-  cardId,
+	imageSource,
+	cardTitle,
+	cardDescription,
+	discountedPrice,
+	discountRate,
+	cardId,
 }: CardProps) => {
-  const router = useRouter();
+	const router = useRouter();
 
-  const handleCardClick = () => {
-    router.push(`/product/${cardId}`);
-  };
-  return (
-    <div className="border-2 rounded-md border-black h-[25rem] w-[15rem] shadow-custom hover:shadow-underlay dark:hover:shadow-darkUnderlay transition-all duration-500 ease-in-out text-center group p-2">
-      <div className="overflow-hidden">
-        <Image
-          height={250}
-          width={250}
-          alt="product"
-          loading="lazy"
-          src={imageSource}
-          className="transition-transform duration-500 ease-in-out brightness-100"
-        />
-      </div>
+	const handleCardClick = () => {
+		router.push(`/product/${cardId}`);
+	};
+	return (
+		<div className="border-2 rounded-md border-black h-[25rem] w-[15rem] shadow-custom hover:shadow-underlay dark:hover:shadow-darkUnderlay transition-all duration-500 ease-in-out text-center group p-2">
+			<div className="overflow-hidden">
+				<Image
+					height={250}
+					width={250}
+					alt="product"
+					loading="lazy"
+					src={imageSource}
+					className="transition-transform duration-500 ease-in-out brightness-100"
+				/>
+			</div>
 
-      <div>
-        <p className="font-semibold">{cardTitle}</p>
-        <p className="text-xs italic">{cardDescription}</p>
-      </div>
+			<div>
+				<p className="font-semibold">{cardTitle}</p>
+				<p className="text-xs italic">{cardDescription}</p>
+			</div>
 
-      <div className="divider px-2 text-gray-600"></div>
+			<div className="divider px-2 text-gray-600"></div>
 
-      <div className="text-sm flex flex-col justify-center items-center gap-5">
-        <div className="flex flex-col gap-3">
-          <p className="flex items-center justify-center">
-            {/* <span className="flex items-center text-gray-400 line-through">
-              <Icon icon="tabler:currency-naira" className="text-sm" />
-              <span className="text-xs">{price}</span>
-            </span> */}
-            <span className="flex items-center">
-              <Icon icon="tabler:currency-naira" className="text-lg" />
-              <span className="text-base font-bold">{discountedPrice}</span>
-            </span>
-          </p>
+			<div className="text-sm flex flex-col justify-center items-center gap-5">
+				<div className="flex flex-col gap-3">
+					<p className="flex items-center justify-center">
+						<span className="flex items-center">
+							<Icon icon="tabler:currency-naira" className="text-lg" />
+							<span className="text-base font-bold">{discountedPrice}</span>
+						</span>
+					</p>
 
-          <div>
-            <p>
-              Enjoy <span className="text-red-600">{discountRate}%</span> off!
-            </p>
-            <span>Offer valid till May 1.</span>
-          </div>
-        </div>
+					<div>
+						<p>
+							Enjoy <span className="text-red-600">{discountRate}%</span> off!
+						</p>
+						<span>Offer valid till May 1.</span>
+					</div>
+				</div>
 
-        <CustomButton
-          onClick={handleCardClick}
-          buttonText="Order Now"
-          buttonSize="btn-sm"
-          buttonTypeOne
-        />
-      </div>
-    </div>
-  );
+				<CustomButton
+					onClick={handleCardClick}
+					buttonText="Order Now"
+					buttonSize="btn-sm"
+					buttonTypeOne
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default Card;
